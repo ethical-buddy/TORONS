23711b727e81dd8a35da511915cdbc30992e5365
A91y <65825207+A91y@users.noreply.github.com>
2024-01-18 14:24:53 +0530

    Refactor web crawler and add socket functionality

--- webapp/app.py
+++ webapp/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, send_from_directory
-import os
+from flask_socketio import SocketIO
+import os, time
 import csv
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 def read_csv(file_path):
     data = []
     with open(file_path, newline='', encoding='utf-8') as csvfile:
@@ -17,6 +19,14 @@ def index():
     file_path = 'data/hateful_files.csv'  # Update with your actual CSV file path
     data = read_csv(file_path)
     return render_template('index.html', data=data)
+
+@socketio.on('update_data')
+def update_data():
+    while True:
+        time.sleep(5)  # Adjust the sleep duration as needed
+        file_path = 'data/hateful_files.csv'
+        data = read_csv(file_path)
+        socketio.emit('data_updated', {'data': data})
 
 @app.route('/archive/<filename>')
 def view_file(filename):
