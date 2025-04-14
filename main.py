e6088f741d0533d52d038ffad64ee89eb2baea94
ambar chakravartty <amch9605@gmail.com>
2025-04-14 16:09:25 +0530

    some experiments in multiprocessing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from colorama import Fore, Style
-from async_crawl4 import main as tor_main
+from async_crawl4 import start_crawlers as tor_main
 from async_crawl_i2p import main as i2p_main
 import os
 import sys
@@ -86,6 +86,7 @@ def main():
                     f"\n{Fore.YELLOW}Starting web crawling through Tor...{Style.RESET_ALL}")
                 command = f"{sys.executable} {tor_file}"
                 open_new_terminal(command)
+                # subprocess.Popen([sys.executable, tor_file])
             elif choice == "2":
                 print(
                     f"\n{Fore.YELLOW}Starting web crawling through I2P...{Style.RESET_ALL}")
